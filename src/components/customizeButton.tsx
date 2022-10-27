0edf5c905917569d968413b88d2a52dc9fae6809
MaltaLiz <lizlrmalta@gmail.com>
2022-10-27 18:21:02 -0300

    melhorando o botão

--- src/components/customizeButton.tsx
+++ src/components/customizeButton.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons'
 import {
   StyleSheet,
   TouchableOpacity,
@@ -6,32 +7,47 @@ import {
 import { colors, dimension } from '../contants'
 import Text from './customizeText'
 
+interface CustomizeButtonProps extends TouchableOpacityProps {
+  add?: boolean
+  small?: boolean
+  red?: boolean
+}
+
 export default function CustomizeButton({
+  add = false,
+  small = false,
+  red = false,
   children,
   style,
   onPress,
-}: TouchableOpacityProps) {
+}: CustomizeButtonProps) {
+  const customizeStyles = styles(small, red)
   return (
-    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-      <Text style={styles.text} type="medium">
-        {children}
+    <TouchableOpacity style={[customizeStyles.button, style]} onPress={onPress}>
+      <Text style={customizeStyles.text} type="medium">
+        {add ? (
+          <Ionicons name="add-circle" color={colors.white} size={32} />
+        ) : (
+          children
+        )}
       </Text>
     </TouchableOpacity>
   )
 }
 
-const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    backgroundColor: colors.blue,
-    width: dimension.width - 32,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 22,
-    lineHeight: 42,
-    textAlign: 'center',
-  },
-})
+const styles = (small: boolean, red: boolean) =>
+  StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 10,
+      backgroundColor: red ? colors.red : colors.blue,
+      width: small ? (dimension.width - 48) / 2 : dimension.width - 32,
+    },
+    text: {
+      color: '#fff',
+      fontSize: 22,
+      lineHeight: 42,
+      textAlign: 'center',
+    },
+  })
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,11 +1,13 @@
-import Button from '../../components/customizeButton'
+import CustomizeButton from '../../components/customizeButton'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { RootStackScreenProps } from '../../types'
 
 export default function Login({ navigation }: RootStackScreenProps<'Login'>) {
   return (
     <SafeAreaView>
-      <Button onPress={() => navigation.navigate('Root')}>Entrar</Button>
+      <CustomizeButton onPress={() => navigation.navigate('Root')}>
+        Entrar
+      </CustomizeButton>
     </SafeAreaView>
   )
 }
