ce2039656919bdb979cd71e758e80305636b5e46
MaltaLiz <lizlrmalta@gmail.com>
2022-10-30 23:31:35 -0300

    configurando o metodo post

--- src/application/request.ts
+++ src/application/request.ts
@@ -1,6 +1,28 @@
+import { FixedReleases } from './types'
+
+const domain = 'http://192.168.0.180:3000'
+
 export async function get(path: string) {
   try {
-    const response = await fetch(`http://192.168.0.180:3000/${path}`)
+    const response = await fetch(`${domain}/${path}`)
+    const json = await response.json()
+    return json
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+export async function post(path: string, body: FixedReleases) {
+  try {
+    const settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }
+    const response = await fetch(`${domain}/${path}`, settings)
     const json = await response.json()
     return json
   } catch (error) {
